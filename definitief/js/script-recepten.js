50b858a84d597965630f10ae1229f0112a7c2695
didierpetit <didier.petit@student.ap.be>
2024-01-03 10:56:05 +0100

    final version

--- definitief/js/script-recepten.js
+++ definitief/js/script-recepten.js
@@ -1,21 +1,21 @@
-fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=9ade122b896a44618e77c3a321e133cd&query=bread')
+fetch('https://api.spoonacular.com/food/search?apiKey=9ade122b896a44618e77c3a321e133cd&query=bread&number=30')
     .then(response => response.json())
     .then(data => {
-        const recipes = data.results || data; // Controleer verschillende mogelijke API-structuren
+        const recipes = data.searchResults.find(result => result.name === 'Recipes').results || [];
         let html = '<div class="accordion accordion-flush" id="accordionExample">';
 
         for (let recipe of recipes) {
             html += `<div class="accordion-item">
                         <h2 class="accordion-header">
                             <button class="accordion-button collapsed text-uppercase fs-6 fw-bolder pt-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${recipe.id}" aria-expanded="false" aria-controls="collapse${recipe.id}">
-                                ${recipe.title}
+                                ${recipe.name}
                             </button>
                         </h2>
                         <div id="collapse${recipe.id}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                             <div class="accordion-body">
                                 <img src="${recipe.image}" style="max-height: 150px;" class="float-start img-sm p-3" alt="">
-                                <span class="h6 mt-2">ingrediënten</span>: ${getIngredients(recipe)} <hr>
-                                <span class="h6 mt-2">bereiding</span>: ${recipe.instructions || 'Geen instructies beschikbaar.'}
+                                <p><strong>Informatie:</strong> ${recipe.content}</p>
+                                <p><strong>Link:</strong> <a href="${recipe.link}" target="_blank">${recipe.name}</a></p>
                             </div>
                         </div>
                     </div>`;
@@ -26,13 +26,3 @@ fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=9ade122b896a4461
     })
     .catch(error => console.error('Error fetching data:', error));
 
-// Functie om ingrediënten op te halen op basis van de API-structuur
-function getIngredients(recipe) {
-    if (recipe.extendedIngredients) {
-        return recipe.extendedIngredients.map(ingredient => ingredient.originalString).join(', ');
-    } else if (recipe.ingredients) {
-        return recipe.ingredients.join(', ');
-    } else {
-        return 'Geen ingrediënten beschikbaar.';
-    }
-}
